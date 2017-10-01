a3f69d81778e8cf7e517d2b2112b3d4c1a1e6fa3
zdwweb <15175529059@163.com>
2017-10-01 12:22:33 +0800

    Update main.py

--- hw03/main.py
+++ hw03/main.py
@@ -15,6 +15,7 @@ def Rectangle():
     end_fill()
 def draw_star(radius,star_x,star_y):
     color('yellow','yellow')
+    speed(10)
     goto(star_x,star_y)
     pt=[]
     pt.append(pos())
@@ -23,57 +24,39 @@ def draw_star(radius,star_x,star_y):
         circle(radius,72)
         pt.append(pos())
         i+=1
-    speed(6)
+    circle(radius,72)
     begin_fill()
     goto(pt[0])
     pendown()
     for i in [3,1,4,2,0]:
         goto(pt[i])
     end_fill()
-Rectangle()
-#主星
-penup()
-setheading(0)
-goto(-340,180)
-left(90)
-forward(32*3)
-left(90)
-draw_star(96,xcor(),ycor())
-#星一
-penup()
-setheading(0)
-goto(-340,180)
-left(atan(3/5)*180//pi)
-goto(-180,276)#小星中心
-backward(32)
-right(90)
-draw_star(32,xcor(),ycor())
-#星二
-penup()
-setheading(0)
-goto(-340,180)
-left(atan(1/7)*180//pi)
-goto(-116,212)
-backward(32)
-right(90)
-draw_star(32,xcor(),ycor())
-#星三
-penup()
-setheading(0)
-goto(-340,180)
-right(atan(2/7)*180//pi)
-goto(-116,116)
-backward(32)
-right(90)
-draw_star(32,xcor(),ycor())
-#星四
-penup()
-setheading(0)
-goto(-340,180)
-right(atan(4/5)*180//pi)
-goto(-180,52)
-backward(32)
-right(90)
-draw_star(32,xcor(),ycor())
-hideturtle()
-
+def found_small_star(angle,star_center):
+    #此函数的作用是找到副星的起始位置
+    penup()
+    setheading(0)
+    goto(-340,180)
+    left(angle)
+    goto(star_center)
+    backward(32)
+    right(90)
+    draw_star(32,xcor(),ycor())
+def found_big_star():
+    #此函数的作用是找到主星的起始位置
+    penup()
+    setheading(0)
+    goto(-340,180)
+    left(90)
+    forward(32*3)
+    left(90)
+    draw_star(96,xcor(),ycor())
+def main():
+    Rectangle()
+    found_big_star()
+    found_small_star(atan(3/5)*180/pi,(-180,276))
+    found_small_star(atan(1/7)*180/pi,(-116,212))
+    found_small_star(-atan(2/7)*180/pi,(-116,116))
+    found_small_star(-atan(4/5)*180/pi,(-180,52))
+    hideturtle()
+if __name__ == '__main__':
+    main()
